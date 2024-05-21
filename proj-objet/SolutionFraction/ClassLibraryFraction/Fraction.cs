19546577b1dc1b2db44eed9c5cf78d4f0ecaab76
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-05-21 16:45:07 +0200

    fin de journée

--- proj-objet/SolutionFraction/ClassLibraryFraction/Fraction.cs
+++ proj-objet/SolutionFraction/ClassLibraryFraction/Fraction.cs
@@ -37,7 +37,7 @@ namespace ClassLibraryFraction
         // afficher
         public override string ToString()
         {
-            return "le numérateur est : " + numerateur + " et le dénominateur est : " + denominateur + "ils sont séparés par une division.";
+            return "le numérateur est : " + numerateur + " et le dénominateur est : " + denominateur + " ils sont séparés par une division.";
         }
         
         // representation textuelle
@@ -55,31 +55,49 @@ namespace ClassLibraryFraction
         }
 
         // oppose 1        
-        public Fraction Oppose()
+        /*
+        public Fraction Oppose() //pour une fonction
         {
             return new Fraction(-(this.numerateur), this.denominateur);
         }
-        
+        */
         // oppose 2
-        /*
-        public void Oppose()
+        
+        public void Oppose() //pour une procedure
         {
             this.numerateur = -this.numerateur;            
         }
-        */
 
-        // inverse
-
+        // inverse 1
+        /*
         public Fraction Inverse()
         {
             return new Fraction(this.denominateur, this.numerateur);
         }
+        */
+        //inverse 2
+
+        public void Inverse() //pour une procedure
+        {
+            int c;                                     
+            c = this.numerateur;
+            this.numerateur = this.denominateur;
+            this.denominateur = c;
+        }
+
+        //inverse 3
+        /*
+        public void Inverse() // sucre syntaxique
+        {
+            (this.numerateur, this.denominateur) = (this.denominateur, this.numerateur);
+        }
+        */
 
         // superieurA
-
-        public bool SuperieurA(Fraction f1)
+        /*
+        public bool SuperieurA(Fraction f)
         {
-            if ((double)this.numerateur / this.denominateur > (double)f1.numerateur / f1.denominateur)
+            if ((double)this.numerateur / this.denominateur > (double)f.numerateur / f.denominateur)
             { 
                 return true;
             }
@@ -88,9 +106,19 @@ namespace ClassLibraryFraction
                 return false; 
             }
         }
+        */
+
+        // superieurA2 // permet moins d'opérations pour le compileur : moins lourd...
+       
+        public bool SuperieurA(Fraction f1)
+        {
+            return (double)this.numerateur / this.denominateur > (double)f1.numerateur / f1.denominateur;
+        }
+        
+
 
         // egalA
-
+        /*
         public bool EgalA(Fraction f1)
         {         
             if ((double)this.numerateur / this.denominateur == (double)f1.numerateur / f1.denominateur)
@@ -102,11 +130,18 @@ namespace ClassLibraryFraction
                 return false;
             }
         }
+        */
+        // superieurA2 // permet moins d'opérations pour le compileur : moins lourd...
+
+        public bool EgalA(Fraction f1)
+        {
+            return (double)this.numerateur / this.denominateur == (double)f1.numerateur / f1.denominateur;
+        }
 
         // reduire
 
 
 
-         
+
     }
 }
--- proj-objet/SolutionFraction/FractionApp/Program.cs
+++ proj-objet/SolutionFraction/FractionApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ClassLibraryFraction;
+using System.Drawing;
 
 namespace FractionApp
 {
@@ -6,7 +7,30 @@ namespace FractionApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Fraction b = new Fraction(8,12);         
+            b.Oppose();
+            Console.WriteLine(b);
+            b.Inverse();
+            Console.WriteLine(b);
+            Fraction d = new Fraction(9, 12);
+            Console.WriteLine(b.SuperieurA(d));
+            /*
+            Point a = new Point();
+            Point a1 = new Point(3, 2);
+            Point a2 = new Point(a1);
+            Point a3 = new Point(7, 9);
+            Point a4 = new Point(9, 10);
+            Point a5 = new Point(11, 13);
+            Point a6 = new Point(12, 14);
+            Console.WriteLine(a1);
+            Console.WriteLine(a1.ToString());
+            Console.WriteLine(a2);
+            a1.Deplacer(6, 8);
+            Console.WriteLine(a1.ToString());
+            Point cloneA3 = a3.SymetrieAxeAbscisse();
+            Console.WriteLine("Pour le point A3 " + cloneA3);
+            */
         }
     }
 }
