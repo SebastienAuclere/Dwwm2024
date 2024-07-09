f4d2514ba40bf86ecd8c9d547e8686415c7a682e
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-07-09 16:54:40 +0200

    message x

--- dossierJavascript/manipuler-un-tableau-ex-n1/assets/js/main.js
+++ dossierJavascript/manipuler-un-tableau-ex-n1/assets/js/main.js
@@ -1,56 +1,40 @@
-// let response = await fetch('./resultat10000metres.json');
-// const resultats = await response.json();
-
-// for (const coureur of resultats) {
-//     const tr = document.createElement("tr");
-
-//     const td1 = document.createElement("td");
-//     td1.textContent = coureur.pays;
-//     tr.appendChild(td1);
-
-//     const td2 = document.createElement("td");
-//     let nomDuCoureur = coureur.nom;
-//     nomDuCoureur = nomDuCoureur.split(' ');
-//     nomDuCoureur = nomDuCoureur[0];
-//     td2.textContent = nomDuCoureur;
-//     tr.appendChild(td2);
-
-//     const td3 = document.createElement("td");
-//     let prenomDuCoureur = coureur.nom;
-//     prenomDuCoureur = prenomDuCoureur.split(' ');
-//     prenomDuCoureur = prenomDuCoureur[1];
-//     td3.textContent = prenomDuCoureur;
-//     tr.appendChild(td3);
-
-//     const td4 = document.createElement("td");
-//     td4.textContent = (coureur.temps / 60).toFixed(0) + "min" + (coureur.temps % 60) + "s";
-//     tr.appendChild(td4);
-//     document.querySelector("tbody").appendChild(tr);
-
 let tabNom = ["Mike Dev", "John Makenzie", "Léa Grande"];
 let maliste = document.querySelector('.liste');
-let monTableau = document.querySelector('.table1');
+
 
 for (const element of tabNom) {
 
     let monItem = document.createElement("li");
     monItem.textContent = element;
     maliste.appendChild(monItem);
-}     
-
-function remplirChamp() {          
-
-    for (const element2 of tabNom) {
-        
-        let champ = document.createElement("td");
-        // element2 = element2.split(' ');
-        // element2 = element2[1];
-        champ.textContent = element2;        
-        monTableau.appendChild(champ);
-    }
-    let tr = document.createElement("tr");
-    tr.appendChild(td);
-    const tbody = document.createElement("tbody");
-    tbody.appendChild(tr);     
 }
+
+function genererTableau() {
+    let myTable = document.querySelector('#listNom');
+    let myBody = myTable.createTBody();
+    myBody.setAttribute('id', 'bodyNom');
+    for (let i = 0; i < tabNom.length; i++) {
+        let myRow = myBody.insertRow();
+        let cellNom = myRow.insertCell();
+        let montabNomPrenom = tabNom[i].split(' ');
+        cellNom.textContent = montabNomPrenom[1];
+        let cellPrenom = myRow.insertCell();
+        cellPrenom.textContent = montabNomPrenom[0];
+        let cellEmail = myRow.insertCell();        
+        cellEmail.textContent = montabNomPrenom[0].toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") + "." +
+            montabNomPrenom[1].toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") + "@example.com";
+        let cellSupprimer = myRow.insertCell();
+        cellSupprimer.setAttribute("style", "text-align:center;font-weight:bold");// cette ligne permet d'ajouter un attribut et apres une 
+        //virgule sa propriété ainsi qu'une valeur, attention mettre ":" pour du javascript.
+        cellSupprimer.textContent = "X";
+        //.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        // STOP: Ce morceau de code sert a enlever tous les caractères spéciaux et mets toutes les lettres en lowercase. 
+        cellSupprimer.setAttribute('id', 'btnx' + i);
+        //maintenant je dois cibler le tr pour qu'en appuyant sur x j'efface le tr
+    }
+}
+
+genererTableau();
+
     
+
