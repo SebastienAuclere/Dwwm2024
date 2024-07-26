d58a3aa3401504ccae696a43a97624eefb1b482d
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-07-26 11:58:41 +0200

    message graffi

--- dossierJavascript/levaluation/assets/js/main.js
+++ dossierJavascript/levaluation/assets/js/main.js
@@ -1,9 +1,8 @@
 let response = await fetch('./eval.json');
 const table = await response.json();
-let notes = 0
-let nbreEleves = 0
-let resultat = 0
 
+let ajouterLigne = document.getElementById("btn1");
+let myBody = document.querySelector('#bodyNom');
 
 table.sort((a, b) => b.grade - a.grade);
 
@@ -27,35 +26,111 @@ for (const eleve of table) {
     const td3 = document.createElement("td");
     td3.textContent = eleve.grade;
     tr.appendChild(td3);
+
+    const td4 = document.createElement("td");
+    let obtenu = evaluationObtenu(eleve.grade);
+    td4.textContent = obtenu;
+    tr.appendChild(td4);
     document.querySelector("tbody").appendChild(tr);
 }
 
+
+
+ajouterLigne.addEventListener('click', function () {
+    let nomPrenom = document.getElementById("nomPrenom").value;
+    console.log(nomPrenom)
+    nomPrenom = nomPrenom.split(" ");
+    let prenom = nomPrenom[1];
+    let nom = nomPrenom[0];
+    let note = document.getElementById("note").value;
+    let obtenu = evaluationObtenu(note);
+
+    if (verifLongueur(nom) && verifLongueur(prenom) && isAlpha(nom) && isAlpha(prenom)) {
+        ajouterUneLigne(nom, prenom, note, obtenu);
+
+        const roland = {
+            fullname: nomPrenom,
+            grade: note
+        }
+        table.push(roland);
+        console.log(table);
+
+    }
+    else {
+
+        nbCharacterMin();
+    }
+
+    monItem.textContent = nbreEtudiants();
+    monItem2.textContent = "Moyenne de la classe : " + moyenneClass();
+    monItem3.textContent = auDessusMoyenne();
+})
+
+
+function verifLongueur(mot) { 
+    if (mot.length < 2) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function isAlpha(str) {
+    return /^[a-zA-Z]{2,}$/.test(str);
+}
+
+function nbCharacterMin() {
+    const para = document.getElementById("ajoutpara");
+    para.innerHTML = '';
+    const ajout = document.createElement("p");
+    ajout.textContent = "Attention : Votre nom et prenom doivent contenir 2 lettres au minimum !"
+    para.appendChild(ajout);
+}
+
+function ajouterUneLigne(nom, prenom, note, obtenu) {
+    let nouvelleLigne = myBody.insertRow();
+    let nouvelleCellule1 = nouvelleLigne.insertCell(); // Nom
+    nouvelleCellule1.textContent = nom;
+    let nouvelleCellule2 = nouvelleLigne.insertCell(); // Prenom
+    nouvelleCellule2.textContent = prenom;
+    let nouvelleCellule3 = nouvelleLigne.insertCell(); // Note
+    nouvelleCellule3.textContent = note;
+    let nouvelleCellule4 = nouvelleLigne.insertCell(); // Obtenu
+    nouvelleCellule4.textContent = obtenu;
+}
+
 function nbreEtudiants() {
-    
     return "Nombre d'étudiants : " + table.length;
 }
 
 function moyenneClass() {
+    let notes = 0
     for (let eleve of table) {
-        notes += eleve.grade
+        notes = notes + eleve.grade
     }
-    return notes / table.length;
+    return (notes / table.length).toFixed(2);
 }
 
-
-
 function auDessusMoyenne() {
-    const moyenne = moyenneClass()
+    let moyenne = moyenneClass()
     let i = 0;
-    for (let eleve of table) {        
-        if (+eleve.grade > moyenne) {            
+    for (let eleve of table) {
+        if (eleve.grade > moyenne) {
             i++
-        }                
+        }
     }
     return "Nombre d'étudiants au dessus de la moyenne : " + i;
 }
- 
 
+function evaluationObtenu(grade) {
+    if (grade >= 12) {
+        return "Oui";
+    }
+    else {
+        return "Non";
+    }
+}
 
 let maListe = document.getElementById("list");
 let monItem = document.createElement("li");
@@ -69,4 +144,8 @@ maListe.appendChild(monItem2);
 let monItem3 = document.createElement("li");
 monItem3.textContent = auDessusMoyenne();
 maListe.appendChild(monItem3);
+
+let monItem4 = document.createElement("li");
+monItem4.textContent = "Note éliminatoire : 12"
+maListe.appendChild(monItem4);
 
