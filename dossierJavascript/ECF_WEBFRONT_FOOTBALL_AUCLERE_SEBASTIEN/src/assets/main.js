fe795465bfd627f1684c377e2bd18bb17b9b1116
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-07-31 12:00:51 +0200

    ok

--- dossierJavascript/ECF_WEBFRONT_FOOTBALL_AUCLERE_SEBASTIEN/src/assets/main.js
+++ dossierJavascript/ECF_WEBFRONT_FOOTBALL_AUCLERE_SEBASTIEN/src/assets/main.js
@@ -1,8 +1,6 @@
 let response = await fetch('./tournoi/football.json');
 const table = await response.json();
 
-console.log(table)
-
 // const equipes = document.getElementById("pEquipes")
 // equipes.textContent = nbreEquipes();
 
@@ -19,9 +17,25 @@ function nbreMatchJoues() {
     return table.length + " matchs joués"
 }
 
-// for (const equipe of table) {
-//     const tr = document.
-// }
+for (const equipe of table) {
+    const tr = document.createElement("tr");
+
+    const td1 = document.createElement("td");
+    td1.textContent = equipe.team1;
+    tr.appendChild(td1);
+
+    const td2 = document.createElement("td");
+    td2.textContent = equipe.team2;
+    tr.appendChild(td2);
+
+    // const td3 = document.createElement("td");    
+    // let score = equipe.score[0];   
+    // td3.textContent = score;
+    // tr.appendChild(td3);
+    document.getElementById("donneeTableau")
+    
+
+}
 
 // for (const eleve of table) {
 //     const tr = document.createElement("tr");
@@ -32,6 +46,9 @@ function nbreMatchJoues() {
 //     nomDeEleve = nomDeEleve[0]
 //     td1.textContent = nomDeEleve;
 //     tr.appendChild(td1);
+
+// tr.appendChild(td4);
+// document.querySelector("tbody").appendChild(tr);
 
 
 
