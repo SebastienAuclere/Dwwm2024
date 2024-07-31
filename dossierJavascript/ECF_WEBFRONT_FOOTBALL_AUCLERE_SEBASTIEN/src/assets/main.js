8d9af2b9e41985af06f35173c9ebd7d5f4fa397f
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-07-31 15:53:07 +0200

    ECF_WEBFRONT_FOOTBALL

--- dossierJavascript/ECF_WEBFRONT_FOOTBALL_AUCLERE_SEBASTIEN/src/assets/main.js
+++ dossierJavascript/ECF_WEBFRONT_FOOTBALL_AUCLERE_SEBASTIEN/src/assets/main.js
@@ -1,14 +1,19 @@
 let response = await fetch('./tournoi/football.json');
 const table = await response.json();
 
-// const equipes = document.getElementById("pEquipes")
-// equipes.textContent = nbreEquipes();
+const equipes = document.getElementById("pEquipes")
+equipes.textContent = nbreEquipes() + " équipes";
 
-// function nbreEquipes() {
-//     for (let ville of table) {
-        
-//     }
-// }
+function nbreEquipes() {
+    for (let i = 0; i < table.length; i++) {
+            if (table.team1 = table.team1) {
+                return 0
+            }
+            else {
+                return i++
+            }                
+    }
+}
 
 const matchs = document.getElementById("pMatch")
 matchs.textContent = nbreMatchJoues();
@@ -28,27 +33,51 @@ for (const equipe of table) {
     td2.textContent = equipe.team2;
     tr.appendChild(td2);
 
-    // const td3 = document.createElement("td");    
-    // let score = equipe.score[0];   
-    // td3.textContent = score;
-    // tr.appendChild(td3);
-    document.getElementById("donneeTableau")
+    const td3 = document.createElement("td");
+    let score = equipe.score;
+    let score1 = score[0];
+    td3.textContent = score1;
+    tr.appendChild(td3);  
     
+    const td4 = document.createElement("td");
+    td4.textContent = gagnant();
+    tr.appendChild(td4);    
 
+    const td5 = document.createElement("td");
+    let score2 = equipe.score;
+    score2 = score[1];    
+    td5.textContent = score2;
+    tr.appendChild(td5);
+
+    const td6 = document.createElement("td");
+    td6.textContent = gagnant();
+    tr.appendChild(td6)    
+    document.getElementById("donneeTableau").appendChild(tr);
 }
 
-// for (const eleve of table) {
-//     const tr = document.createElement("tr");
+function gagnant() {
+    for (const equipe of table) {
+        let resultat = equipe.score;
+        // resultat = resultat.split('-');
+        // console.log(resultat)
+        let resultat1 = resultat[0];
+        // console.log(resultat[0])
+        let resultat2 = resultat[1];
+        // console.log(resultat[1])
+        if (resultat1 > resultat2) {            
+            return equipe.team1            
+        }
+        else if (resultat1 = resultat2) {
+            return "égalité"
+        }
+        else {
+            return equipe.team2
+        }        
+    }    
+}
 
-//     const td1 = document.createElement("td");
-//     let nomDeEleve = eleve.fullname;
-//     nomDeEleve = nomDeEleve.split(' ');
-//     nomDeEleve = nomDeEleve[0]
-//     td1.textContent = nomDeEleve;
-//     tr.appendChild(td1);
 
-// tr.appendChild(td4);
-// document.querySelector("tbody").appendChild(tr);
+
 
 
 
